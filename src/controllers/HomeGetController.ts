fca66b787bb37f326c9a904d892231ded10b54be
dimaslz <me@dimaslz.io>
2023-02-04 13:18:14 +0100

    feat(data): access to attribute

--- src/controllers/HomeGetController.ts
+++ src/controllers/HomeGetController.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
+import camelCase from "lodash.camelcase";
 
 import { Controller } from "./Controller";
 import data from "./data";
 
 export default class HomeGetController implements Controller {
-	async run(req: Request, res: Response) {
+	async run(req: Request<{ attribute: string }, never, never, never>, res: Response) {
 		res.status(httpStatus.OK);
+
+		const { attribute } = req.params;
+
+		if (!(camelCase(attribute) in data)) {
+			res.json({
+				status: 404,
+				message: "Resource data not found",
+			});
+		} else {
+			return res.json(data[camelCase(attribute)]);
+		}
 
 		res.json({ ...data });
 	}
--- src/routes/home.route.ts
+++ src/routes/home.route.ts
@@ -5,5 +5,5 @@ import HomeGetController from "@/controllers/HomeGetController";
 export const register = (app: Express) => {
 	const homeGetController: HomeGetController = new HomeGetController();
 
-	app.get("/", homeGetController.run.bind(homeGetController));
+	app.get("/:attribute?", homeGetController.run.bind(homeGetController));
 };
